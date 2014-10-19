958f6c4a33b32ff54f699f1797f58dc5bc093c28
Grant Carroll <gcarroll2603@gmail.com>
2014-10-18 21:02:36 -0400

    Added SQL to create the table

---
+++ SQL/CreateTables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE MagicCards
+(
+    CardName varchar(255),
+    ManaCost varchar(255),
+    Types varchar(255),
+    CardText varchar(MAX),
+    CreaturePower int,
+    CreatureToughness int,
+    PlaneswalkerLoyalty int
+);
