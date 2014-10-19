137fdbff1ddea06b3b0f3c497405873d4bcf33ec
Grant Carroll <gcarroll2603@gmail.com>
2014-10-19 11:12:02 -0400

    Added Logic for checking Search and Add Buttons

--- CardAdd.php
+++ CardAdd.php
@@ -95,54 +95,8 @@ margin-right: auto;
 </div>
 <br>
 <h1 class = "search">
-    Card Search
+    Added Successfully!!!
 </h1>
-<div class = "searchbox" >
-<form class = "entry" action="SearchOrAddProcessing.php" method="post">
-    Card Name: <div><input type="text" name="cardname"></div>
-    <br>
-    Mana Cost: <div><input type="text" name="manacost"></div>
-    <br>
-    Types: <div><input type="text" name="types"></div>
-    <br>
-    Card Text: <div><input type="text" name="cardtext"></div>
-    <br>
-    Power: <div><input type="text" name="power"></div>
-    <br>
-    Toughness: <div><input type="text" name="toughness"></div>
-    <br>
-    Loyalty: <div><input type="text" name="loyalty"></div>
-    <br>
-    <input type="submit" value="Search" name="Search">
-    <input type="submit" value="Add" name="Add">
-</form>
-</div>
-<br>
-<h1 class = "search">
-Result
-</h1>
-<div class = "searchbox" >
-<div class = "result" >
-    Card Name: php echo $cardDetails["CardName"];
-    <br>
-    Mana Cost: <?php echo $cardDetails["ManaCost"]; ?>
-    <br>
-    Types: <?php echo $cardDetails["Types"]; ?>
-    <br>
-    Card Text: <?php echo $cardDetails["CardText"]; ?>
-    <br>
-    Power: <?php echo $cardDetails["CreaturePower"]; ?>
-    <br>
-    Toughness: <?php echo $cardDetails["CreatureToughness"]; ?>
-    <br>
-    Loyalty: <?php echo $cardDetails["PlaneswalkerLoyalty"]; ?>
-    <form>
-    <input type="submit" value="Edit">
-    <input type="submit" value="Delete">
-    </form>
-    
-</div>
-</div>
 
 </body>
 </html>
---
+++ SearchOrAddProcessing.php
@@ -0,0 +1,10 @@
+<?php
+if ($_POST['Search'])
+{
+    header('Location: MagicPage.php');
+}
+else if ($_POST['Add'])
+{
+    header('Location: CardAdd.php');
+}
+?>
